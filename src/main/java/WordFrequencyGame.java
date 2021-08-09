abfb4fa426c29f57595a9db1f424a5de1a90c581
AMEER JASSER BRIMA (OPS-IRIS-ISD-OOCLL/MNL) <BRIMAAM@OOCL.COM>
2021-08-09 15:49:14 +0800

    [WordFrequencyGame][AmeerBrima] refactor: removed unused method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -31,27 +31,6 @@ public class WordFrequencyGame {
         }
     }
 
-    private List<WordInfo> getWordAndCount(String sentence) {
-        //split the input string with 1 to n pieces of spaces
-        String[] arr = sentence.split(BLANK_SPACE);
-
-        List<WordInfo> wordInfoList = new ArrayList<>();
-        for (String s : arr) {
-            WordInfo wordInfo = new WordInfo(s, 1);
-            wordInfoList.add(wordInfo);
-        }
-
-        //get the map for the next step of sizing the same word
-        Map<String, List<WordInfo>> map = getListMap(wordInfoList);
-
-        List<WordInfo> list = new ArrayList<>();
-        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
-            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-            list.add(wordInfo);
-        }
-        return list;
-    }
-
     private List<WordInfo> calculateWordAndCount(String sentence){
         List<WordInfo> wordInfos  = new ArrayList<>();
         List<String> words = Arrays.asList(sentence.split(BLANK_SPACE));
